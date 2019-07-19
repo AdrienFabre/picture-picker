0a0443ee5dde62f454c7aebf7b03df4d66d6e486
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-07-19 20:10:37 +0100

    refactor

--- lib/picture_picker.rb
+++ lib/picture_picker.rb
@@ -9,11 +9,23 @@ class PicturePicker
   end
 
   def most_similar_picture(picture_selected)
-    targeted_descriptions =
-      data[picture_selected].map do |targeted_data|
-        targeted_data['description']
-      end
+    targeted_descriptions = get_descriptions(picture_selected)
 
+    pictures_descriptions = get_other_descriptions(picture_selected)
+
+    pictures_ranked =
+      rank_similarities(targeted_descriptions, pictures_descriptions)
+
+    pictures_ranked.sort_by { |k, v| v }.reverse.first[0]
+  end
+
+  private
+
+  def get_descriptions(picture_selected)
+    data[picture_selected].map { |targeted_data| targeted_data['description'] }
+  end
+
+  def get_other_descriptions(picture_selected)
     pictures_descriptions = Hash.new(0)
     data.keys.map do |picture|
       next if picture == picture_selected
@@ -23,21 +35,23 @@ class PicturePicker
         pictures_descriptions[picture] << picture_data['description']
       end
     end
+    pictures_descriptions
+  end
 
-    pictures_rank = Hash.new(0)
-
+  def rank_similarities(targeted_descriptions, pictures_descriptions)
+    pictures_ranked = Hash.new(0)
     pictures_descriptions.each do |picture, descriptions|
-      pictures_rank[picture] = 0
+      pictures_ranked[picture] = 0
       targeted_descriptions.each do |description|
         if descriptions.include?(description)
-          if pictures_rank[picture].positive?
-            pictures_rank[picture] += 1
+          if pictures_ranked[picture].positive?
+            pictures_ranked[picture] += 1
           else
-            pictures_rank[picture] = 1
+            pictures_ranked[picture] = 1
           end
         end
       end
     end
-    pictures_rank.sort_by { |k, v| v }.reverse.first[0]
+    pictures_ranked
   end
 end
